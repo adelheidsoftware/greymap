d69efc45e24a93418e28f6eb9626cae31d719e77
Aaron J Yoder <aaronjyoder@gmail.com>
2023-02-12 09:57:40 -0500

    Upload base project

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "org.adelheid"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+
+    implementation("org.spigotmc:plugin-annotations:1.1.0-SNAPSHOT")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/adelheid/greymap/Main.java
@@ -0,0 +1,7 @@
+package org.adelheid.greymap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
